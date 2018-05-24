63eeab407e637d7f28e876f5e76194a72b4443ff
Mark Williams <mark.williams@christie.nhs.uk>
2018-05-24 18:49:10 +0100

    Initial commit

---
+++ CharPlay/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CharPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string testString = " 0123456789 mark ";
+			int i = testString.Length;
+			int actualCharConvertedToInt;
+
+			Console.WriteLine("---------------------------------------------------------");
+			Console.WriteLine(testString);
+			Console.WriteLine("---------------------------------------------------------");
+			Console.WriteLine(i);
+			Console.WriteLine("---------------------------------------------------------");
+			for (int loop = 0; loop < i; loop++)
+			{
+				Console.Write(testString[loop]);
+				Console.Write(" loop = ");
+				Console.Write(loop);
+
+				//Convert the char indexed by 'loop' to an int
+
+				actualCharConvertedToInt = testString[loop];
+				Console.Write(" actualCharConvertedToInt = \t");
+				Console.Write(actualCharConvertedToInt);
+				Console.Write(" -\t");
+				//Console.Write(Char.GetNumericValue('m')); //returns -1
+				Console.WriteLine(Char.GetNumericValue(testString[loop]));
+			}
+			Console.WriteLine("---------------------------------------------------------");
+			Console.ReadKey();
+		}
+    }
+}
+
